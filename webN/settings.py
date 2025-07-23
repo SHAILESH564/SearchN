5e2d68ca3a4efa078eb7c4e47af535186715cbf5
Shailesh <shaileshbmaurya@gmail.com>
2025-07-23 16:20:59 +0530

    setting db

--- webN/settings.py
+++ webN/settings.py
@@ -81,7 +81,8 @@ WSGI_APPLICATION = 'webN.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        # default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default = os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
 }
