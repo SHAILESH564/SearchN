d6eac3cfc8a302a95ddc2a383dbb949696ff3957
Shailesh <shaileshbmaurya@gmail.com>
2025-07-24 20:00:31 +0530

    new changes

--- webN/settings.py
+++ webN/settings.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = 'webN.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default = os.environ.get('postgresql://searchn_user:PEh6ljqMxtlWqddorECJ3CJe14WGs1be@dpg-d20b2sp5pdvs73cbd8k0-a/searchn'),
-#         conn_max_age=600
-#     )
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default = os.environ.get('postgresql://searchn_user:PEh6ljqMxtlWqddorECJ3CJe14WGs1be@dpg-d20b2sp5pdvs73cbd8k0-a/searchn'),
+        conn_max_age=600
+    )
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
